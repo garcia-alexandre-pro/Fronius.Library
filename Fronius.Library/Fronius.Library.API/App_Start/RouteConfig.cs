ff6d455c3c273e372e43395ce67ec25c476632a1
garcia-alexandre-pro <67734538+garcia-alexandre-pro@users.noreply.github.com>
2025-05-18 23:25:52 +0200

    API

--- Fronius.Library/Fronius.Library.API/App_Start/RouteConfig.cs
+++ Fronius.Library/Fronius.Library.API/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Fronius.Library.API
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Book", action = "Get", id = UrlParameter.Optional }
             );
         }
     }
